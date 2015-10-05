b84fcce1bbe4e6b635b1702af090156ab45a1dd0
sfinley89 <sfinley@hovelstay.com>
2015-10-05 14:00:02 -0700

    require sinatra, use rerun

--- yelp_api.rb
+++ yelp_api.rb
@@ -5,3 +5,12 @@ get '/' do
   @events = HTTParty.get "http://mashable.com/stories.json"
   erb :index
 end
+
+
+client = Yelp::Client.new({ consumer_key: P_mTPzx8SaJgKsXrhTO2Pw,
+                            consumer_secret: tz8FV38p8pnFAHXSF9gbzFdOFho,
+                            token: 1QMSpCN_9J73Y5BJ7Okj5Fe1BA5-67iA,
+                            token_secret: JJccYdZGObLWJkL3GUSQDR5eZuU
+                          })
+
+end
