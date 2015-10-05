38d503f7c79a8a1245e3949cc589a229f71c68a2
sfinley89 <sfinley@hovelstay.com>
2015-10-05 14:13:13 -0700

    dont use sinatra or httparty here

--- yelp_api.rb
+++ yelp_api.rb
@@ -1,16 +1,15 @@
-require 'sinatra'
 require 'httparty'
 
 get '/' do
-  @events = HTTParty.get "http://mashable.com/stories.json"
+  @events = HTTParty.get "http://api.yelp.com/v2/search/?term=food&location=Los Angeles, CA&=food&category_filter=food"
   erb :index
 end
 
 
-client = Yelp::Client.new({ consumer_key: P_mTPzx8SaJgKsXrhTO2Pw,
-                            consumer_secret: tz8FV38p8pnFAHXSF9gbzFdOFho,
-                            token: 1QMSpCN_9J73Y5BJ7Okj5Fe1BA5-67iA,
-                            token_secret: JJccYdZGObLWJkL3GUSQDR5eZuU
-                          })
+# client = Yelp::Client.new({ consumer_key: P_mTPzx8SaJgKsXrhTO2Pw,
+#                             consumer_secret: tz8FV38p8pnFAHXSF9gbzFdOFho,
+#                             token: 1QMSpCN_9J73Y5BJ7Okj5Fe1BA5-67iA,
+#                             token_secret: JJccYdZGObLWJkL3GUSQDR5eZuU
+#                           })
 
-end
+# end
