fa6bee383a8b381af3e13060dfb71b670f00c7b3
sfinley89 <sfinley@hovelstay.com>
2015-10-08 20:03:27 -0700

    needs restaurants

--- yelp_api.rb
+++ yelp_api.rb
@@ -1,15 +1,18 @@
-require 'httparty'
-
-get '/' do
-  @events = HTTParty.get "http://api.yelp.com/v2/search/?term=food&location=Los Angeles, CA&=food&category_filter=food"
-  erb :index
-end
+require 'yelp'
 
 
-# client = Yelp::Client.new({ consumer_key: P_mTPzx8SaJgKsXrhTO2Pw,
-#                             consumer_secret: tz8FV38p8pnFAHXSF9gbzFdOFho,
-#                             token: 1QMSpCN_9J73Y5BJ7Okj5Fe1BA5-67iA,
-#                             token_secret: JJccYdZGObLWJkL3GUSQDR5eZuU
-#                           })
 
-# end
+
+client = Yelp::Client.new({ consumer_key: "P_mTPzx8SaJgKsXrhTO2Pw",
+                            consumer_secret: "tz8FV38p8pnFAHXSF9gbzFdOFho",
+                            token: "1QMSpCN_9J73Y5BJ7Okj5Fe1BA5-67iA",
+                            token_secret: "JJccYdZGObLWJkL3GUSQDR5eZuU"
+                          })
+
+results = client.search('Los Angeles')
+
+results.businesses.each do |result|
+	puts result.restraunt.name
+
+	end
+
